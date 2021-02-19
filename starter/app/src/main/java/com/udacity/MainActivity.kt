8c631b0c9bd7c14e8964c5199429c919d6c8aeb3
Vladimir Espinola Lezcano <vladimir.espinola.contractor@bbva.com>
2021-02-18 23:49:17 -0300

    calculated download progress

--- starter/app/src/main/java/com/udacity/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/MainActivity.kt
@@ -9,6 +9,9 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import android.view.View
 import android.widget.RadioButton
 import android.widget.Toast
@@ -26,6 +29,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var pendingIntent: PendingIntent
     private lateinit var action: NotificationCompat.Action
 
+    private var handler = Handler(Looper.getMainLooper())
+    private var isProgressCheckerRunning = false
+
+
     private var selectedURL = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,15 +43,32 @@ class MainActivity : AppCompatActivity() {
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
         custom_button.setOnClickListener {
+            if (selectedURL.isEmpty()) {
+                showToast()
+                return@setOnClickListener
+            }
             download()
         }
 
+        startProgressChecker()
+        showToast()
+    }
+
+    override fun onDestroy() {
+        stopProgressChecker()
+        super.onDestroy()
+    }
+
+    private fun showToast() {
         Toast.makeText(this, getString(R.string.please_select), Toast.LENGTH_LONG).show()
     }
 
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
+            //TODO: NOTIFICATION
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+
+            downloadID = 0
         }
     }
 
@@ -58,10 +82,64 @@ class MainActivity : AppCompatActivity() {
                 .setAllowedOverRoaming(true)
 
         val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
-        downloadID =
-            downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+
+        if (downloadID != 0L) {
+            downloadManager.remove(downloadID)
+        }
+
+        downloadID = downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+    }
+
+    private fun checkDownloadProgress() {
+        val query = DownloadManager.Query()
+        query.setFilterByStatus((DownloadManager.STATUS_FAILED or DownloadManager.STATUS_SUCCESSFUL).inv())
+
+        val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+        val cursor = downloadManager.query(query)
+
+        if (!cursor.moveToFirst()) {
+            cursor.close()
+            return
+        }
+
+        do {
+            val bytes_downloaded = cursor.getInt(cursor
+                    .getColumnIndex(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR))
+            val bytes_total = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_TOTAL_SIZE_BYTES))
+
+            val dl_progress = (bytes_downloaded * 100L / bytes_total).toInt()
+
+            Log.d(MainActivity.javaClass.name, "progress so far: $dl_progress")
+
+        } while (cursor.moveToNext())
+        cursor.close()
 
     }
+
+    private fun startProgressChecker() {
+        if (!isProgressCheckerRunning) {
+            isProgressCheckerRunning = true
+            progressChecker.run()
+        }
+    }
+
+    private fun stopProgressChecker() {
+        handler.removeCallbacks(progressChecker)
+        isProgressCheckerRunning = false
+    }
+
+    private val progressChecker: Runnable = object : Runnable {
+        override fun run() {
+            try {
+                checkDownloadProgress()
+                // manager reference not found. Commenting the code for compilation
+                //manager.refresh();
+            } finally {
+                handler.postDelayed(this, PROGRESS_DELAY.toLong())
+            }
+        }
+    }
+
     fun onRadioButtonClicked(view: View) {
         if (view is RadioButton) {
             // Is the button now checked?
@@ -92,6 +170,8 @@ class MainActivity : AppCompatActivity() {
         private const val GLIDE_URL = "https://github.com/bumptech/glide/archive/master.zip"
         private const val RETROFIT_URL = "https://github.com/square/retrofit/archive/master.zip"
         private const val CHANNEL_ID = "channelId"
+
+        private const val PROGRESS_DELAY = 1000
     }
 
 }
